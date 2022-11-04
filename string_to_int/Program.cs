2d9db170e990854d75c7ec6e0b5f9ebb57487f31
Simian <alex@Mandelbrot>
2022-11-03 22:01:01 -0600

    finished string to int

--- string_to_int/Program.cs
+++ string_to_int/Program.cs
@@ -1,13 +1,10 @@
-﻿using System.Linq;
+﻿// https://leetcode.com/problems/string-to-integer-atoi
+
+using System.Linq;
+Console.WriteLine(myAtoi("21474836460"));
+
+int myAtoi(string s)
 {
-    
-}
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine(myAtoi("   -12334"));
-
-
-
-int myAtoi(string s){
 	Dictionary<char, int> charToInt = new();
 	charToInt.Add('0',0);
 	charToInt.Add('1',1);
@@ -19,21 +16,22 @@ int myAtoi(string s){
 	charToInt.Add('7',7);
 	charToInt.Add('8',8);
 	charToInt.Add('9',9);
+
 	s = s.Trim();
-	//
-		//get the sign if there is one if not assume that it is positive
+	if(s.Length == 0)return 0;
+
 	bool isPositive = true;
 	int start = 0;
 	if(s[0] == '-' || s[0] == '+'){
 		start =1;
 		isPositive = s[0] != '-';
 	}
-	
+
 	List<int> listOfInts = new();
 
 	// Start going the string starting at the left 
-	for(int i = start;i < s.Length; i++){
-
+	for(int i = start;i < s.Length; i++)
+	{
 		if(charToInt.ContainsKey(s[i])){
 			listOfInts.Add(charToInt[s[i]]);
 		}else{
@@ -41,24 +39,28 @@ int myAtoi(string s){
 		}
 	}
 
-	//listOfInts.ForEach (i=> Console.WriteLine(i));
-
+	// Now calculate
 	int result = 0;
-	// now calculate
 	int place = 0;
-	for(int i = listOfInts.Count - 1; i>=0; i--){
-		
-		result += listOfInts[i] *(int)Math.Pow(10,place) ;
+	for(int i = listOfInts.Count - 1; i>=0; i--)
+	{
+		result += (int)(listOfInts[i] *Math.Pow(10,place));
 		place ++;
+
+		if(isPositive && result <0){
+			// Overflow 
+			result =(int) Math.Pow(2,31)-1;
+			break;
+		}
+		if(!isPositive && result < 0){
+			// Overflow
+			result = (int)Math.Pow(2,31) *-1;
+			break;
+		}
 	}
 	if(!isPositive){
 		result = result * -1;
 
 	}
-	// if it is an int save it to the list
-	// if it is a character stop
-	//
-	// go through the built array starting at the right and building the number 
-	// timsing it by it's place
 	return result;	
 }
