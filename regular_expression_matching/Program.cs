8add8535f5035c1fe88f4e595abf0b4e9db0661b
Simian <alex@Mandelbrot>
2022-11-14 21:03:54 -0700

    wrong solution * is zero or more

--- regular_expression_matching/Program.cs
+++ regular_expression_matching/Program.cs
@@ -1,91 +1,71 @@
 ﻿
-Console.WriteLine("Passed: " + (true == IsMatch("a","a")).ToString());
-Console.WriteLine("----------------------------------------");
-Console.WriteLine("Passed: " + (false == IsMatch("aa","a")).ToString());
-Console.WriteLine("----------------------------------------");
-Console.WriteLine("Passed: " + (false == IsMatch("pppppk","p.k")).ToString());
-Console.WriteLine("----------------------------------------");
-Console.WriteLine("Passed: "+ (true == IsMatch("pak","p.k")).ToString());
-Console.WriteLine("----------------------------------------");
-Console.WriteLine("Passed: "+ (true == IsMatch("pppppk","p*k")).ToString());
-Console.WriteLine("----------------------------------------");
-Console.WriteLine("Passed: "+ (true == IsMatch("pppk","...k")).ToString());
-Console.WriteLine("----------------------------------------");
-Console.WriteLine("Passed: "+ (false == IsMatch("","...k")).ToString());
-Console.WriteLine("----------------------------------------");
-Console.WriteLine("Passed: "+ (true == IsMatch("ab",".*")).ToString());
-Console.WriteLine("Passed: "+ (true == IsMatch("mississippi","mis*is*p*.")).ToString());
+Console.WriteLine("Passed: " + (false == IsMatch("aab","c*a*b")).ToString());
+//Console.WriteLine("----------------------------------------");
+//Console.WriteLine("Passed: " + (true == IsMatch("a","a")).ToString());
+//Console.WriteLine("----------------------------------------");
+//Console.WriteLine("Passed: " + (false == IsMatch("aa","a")).ToString());
+//Console.WriteLine("----------------------------------------");
+//Console.WriteLine("Passed: " + (false == IsMatch("pppppk","p.k")).ToString());
+//Console.WriteLine("----------------------------------------");
+//Console.WriteLine("Passed: "+ (true == IsMatch("pak","p.k")).ToString());
+//Console.WriteLine("----------------------------------------");
+//Console.WriteLine("Passed: "+ (true == IsMatch("pppppk","p*k")).ToString());
+//Console.WriteLine("----------------------------------------");
+//Console.WriteLine("Passed: "+ (true == IsMatch("pppk","...k")).ToString());
+//Console.WriteLine("----------------------------------------");
+//Console.WriteLine("Passed: "+ (false == IsMatch("","...k")).ToString());
+//Console.WriteLine("----------------------------------------");
+//Console.WriteLine("Passed: "+ (true == IsMatch("ab",".*")).ToString());
+//Console.WriteLine("Passed: "+ (false == IsMatch("mississippi","mis*is*p*.")).ToString());
 
 
  bool IsMatch(string s, string p)
 {
+// bool log = false;
 	Console.WriteLine($"Trying {s}, {p}");
 	if(!(p.Contains("*") || p.Contains("."))){
 		return s == p;
 	}
 	if(s.Length == 0 && p.Length > 0) return false;
 
-	bool result= true;
-	bool inRepeatingPattern = false;
-	bool wildcard = false;
 
-	int patternLocation = 0;
-	for(int i = 0; i < s.Length; i++){
-		Console.Write($"i = {i}, p = {patternLocation}");
-		if(p[patternLocation] == '.'){
-			// skip pattern
-			Console.Write(" skipping pattern");
-			wildcard = true;		
-			//			if(s.Length -1 > i){
-			//			result = false;
-			//		Console.WriteLine("breaking  pattern");
-			//	break;
-			//}
-		}else{
-			wildcard = false;
-			//			Console.Write(" " + p[patternLocation + 1]+ "");
-			// Look ahead to check for repeating pattern
+	int sLocation =0;
+	for(int i = 0; i < p.Length; i++){
+		// going through the pattern
+		bool hasNext = i + 1 < p.Length;
+
+//		if(log) Console.WriteLine($"i: {i} p: {p[i]} sLoc:{sLocation} s: {s[sLocation]}");
+
+		bool nextIsRepeating =false; 
+		if(hasNext){
+			nextIsRepeating = p[i+1] == '*';
 		}
-		Console.Write($" ploc +1= {patternLocation+1} plength-1 {p.Length -1}");
-		if( !(patternLocation+1 > p.Length-1) && p[patternLocation + 1] == '*'){
-			Console.Write(" repeat start ");
-			inRepeatingPattern = true; 
-			// Move ahead
-			while(s[i] != p[patternLocation]){
-				i++;
-			}
-			i++;
-			patternLocation ++;
 
-		}else{
-
-			Console.Write($" Comp {s[i]} = {p[patternLocation]} wc = {wildcard}");
-			if(!wildcard){
-				if(s[i] != p[patternLocation]){
-					//		if(inRepeatingPattern){
-					//			inRepeatingPattern = false;
-					//			i--;
-					//			patternLocation++;
-					//			Console.Write(" End Of Repeating");
-					//		}else{
-					result = false;
-					Console.Write(" non match found ");
-					break;
-					//	}
+		bool matchAny = p[i] == '.';
+		
+		if(matchAny || s[sLocation] == p[i]){
+			// Found a match 
+			char currentMatch = s[sLocation];
+			if(nextIsRepeating){
+				// Find next one non match character
+				for(int j = sLocation + 1 ; j < s.Length; j++){
+					if(s[j] != currentMatch){
+						// End of match
+						sLocation = j;
+						break;
+					}
 				}
+				i++;
+			}else{
+				sLocation ++;
 			}
-			//	if(!inRepeatingPattern){
-			//		patternLocation++;
-			//		Console.Write($" increment patternLocation {patternLocation}");
-			//	}
+		}else{
+			return false;
 		}
 
-
-		//		Console.Write($"fjdkjfkdj");
-		patternLocation++;
-		Console.WriteLine();
 	}
-	return result;
+	return true;
+
 }
 
 
